9fa81876567a99050454daf4e16b558fc8d11dd2
HjalteMuus <hjaltemb2@gmail.com>
2025-04-03 12:49:12 +0200

    Made first working file maker

--- Files/src/FileExample.java
+++ Files/src/FileExample.java
@@ -1,3 +1,15 @@
-public class FileExample {
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class FileExample {
+    public static void main(String[] args) {
+        File file = new File("./output.txt");
+
+        try (PrintWriter pw = new PrintWriter(file)){
+            pw.println("Hejsa");
+        } catch (IOException e) {
+            System.out.println("Oops");
+        }
+    }
 }
