8ef6000cf39b9dc3af3c5423689d3839da89f33d
HjalteMuus <hjaltemb2@gmail.com>
2025-04-03 13:08:21 +0200

    File maker with reader

--- Files/src/FileExample.java
+++ Files/src/FileExample.java
@@ -1,15 +1,29 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileExample {
     public static void main(String[] args) {
-        File file = new File("./output.txt");
+        File file = new File("./data.txt");
 
         try (PrintWriter pw = new PrintWriter(file)){
-            pw.println("Hejsa");
+            pw.println("Hej dette er en test");
+            pw.println("Her er der en sætning");
+            pw.println("Bruh");
         } catch (IOException e) {
             System.out.println("Oops");
+        }
+
+        try {
+            Scanner s = new Scanner(file);
+            while(s.hasNextLine()){
+                String data = s.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
     }
 }
